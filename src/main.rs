b3c8e1c2817df99c6f801afe220ff31df385911d
nehemie-n <nehemie054@gmail.com>
2023-08-24 15:22:56 +0200

    feat: vectorize macro

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,15 @@
 mod macros;
 
-use core::num;
-
-use macros::{greet, vectorize};
-
 fn main() {
+    
+    // @iterators 
     // Using iterators
     // iterators()
     // simulate()
-    greet!(String::from("Nehemie"));
-    let nums = vectorize!(16);
-    assert_eq!(nums.len(), 16);
-    println!("NUMS {nums:?}")
+    
+    // @macros
+    // greet!(String::from("Nehemie"));
+    // let nums = vectorize!(16);
+    // assert_eq!(nums.len(), 16);
+    // println!("NUMS {nums:?}")
 }
